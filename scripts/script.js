125640e65bec993a123f0300d65808bacc645e3b
Lawson Hung <lawsonhung@yahoo.com>
2025-12-16 13:48:10 -0500

    Append h1 fragment to mainEl

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+let mainEl = document.querySelector('main');
+
+const rootElement = document.documentElement;
+const computedStyles = window.getComputedStyle(rootElement);
+let mainBgColor = computedStyles.getPropertyValue('--main-bg');
+mainEl.style.backgroundColor = mainBgColor;
+
+const fragment = document.createDocumentFragment();
+const h1Element = document.createElement('h1');
+h1Element.textContent = 'DOM Manipulation';
+fragment.appendChild(h1Element);
+mainEl.appendChild(fragment);
